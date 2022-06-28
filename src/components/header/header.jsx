2469423e484f4edfa76500ddcc0c724e5d77187f
Connant <diconna@gmail.com>
2022-06-28 18:39:29 +0700

    added header and swiper

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,14 +1,14 @@
-
+import "./header.scss"
 
 export default function Header() {
 
     return (
-            <header>
-                <div className="logo"></div>
-                <div className="contacts">
-                    <div className="tel">8 800 000 00 00</div>
-                    <div className="mail">sales@logo.ru</div>
-                </div>
-            </header>
+        <header className="page-header">
+            <img src="./img/logo.svg" className="page-header__logo" alt="logo" />
+            <div className="page-header__contacts contacts">
+                <div className="contacts__phone">8 800 000 00 00</div>
+                <div className="contacts__mail">sales@logo.ru</div>
+            </div>
+        </header>
     )
 }
--- src/components/slider/slider.jsx
+++ src/components/slider/slider.jsx
@@ -1,21 +1,56 @@
+import React, { useRef } from "react";
+import { Pagination, A11y } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
 
+// import slider_1 from "../../../public/img/slider_1.png";
 
-export default function Slider() {
+import "swiper/css";
+import "swiper/css/pagination";
+
+import "./slider.scss";
+
+const Slider = () => {
 
     return (
+        <div className="swiper">
+            <Swiper
+                modules={[Pagination, A11y]}
+                slidesPerView={1}
+                pagination={{ clickable: true }}
+            >
+                <SwiperSlide>
+                    <img className="swiper__image" src="./img/slider_1.png" alt="back" />
+                    <div className="swiper__content">
+                        <h2 className="swiper__title">Как бизнесу сохранять IT-кадры на фоне кризиса</h2>
+                        <p className='swiper__text'>
+                        Инструменты, которые могут использовать компании для удержания сотрудников
+                        </p>
+                        <a href="/" className="swiper__button">Подробнее</a>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img className='swiper__image' src="./img/slider_1.png" alt="back" />
+                    <div className='swiper__content'>
+                        <h2 className='swiper__title'>Как бизнесу сохранять IT-кадры на фоне кризиса</h2>
+                        <p className='swiper__text'>
+                        Инструменты, которые могут использовать компании для удержания сотрудников
+                        </p>
+                        <a href="/" className="swiper__button">Подробнее</a>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img className='swiper__image' src="./img/slider_1.png" alt="back" />
+                    <div className='swiper__content'>
+                        <h2 className='swiper__title'>Как бизнесу сохранять IT-кадры на фоне кризиса</h2>
+                        <p className='swiper__text'>
+                        Инструменты, которые могут использовать компании для удержания сотрудников
+                        </p>
+                        <a href="/" className="swiper__button">Подробнее</a>
+                    </div>
+                </SwiperSlide>
+            </Swiper>
+        </div>
+    );
+};
 
-        <section className="sliders">
-            <h1 className="title">
-                Как бизнесу сохранять IT-кадры на фоне кризиса
-            </h1>
-            <p className="text">
-                Инструменты, которые могут использовать компании для удержания сотрудников
-            </p>
-            <button className="button">
-                Подробнее
-            </button>
-        </section>
-
-
-    )
-}
+export default Slider;
