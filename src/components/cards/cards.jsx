03400a2fc840e1a365da4f3c48c93819bee64c16
Connant <diconna@gmail.com>
2022-06-28 19:36:12 +0700

    scss styles of cards

--- src/components/cards/cards.jsx
+++ src/components/cards/cards.jsx
@@ -1,16 +1,61 @@
-
+import React, { useState } from 'react';
+import './cards.scss'
 
 export default function Cards() {
+
+
+    const [dataFrom, setDataFrom] = useState(null);
+    const [dataTill, setDataTill] = useState(null);
+
+    const [value, setValue] = useState('');
+
+    const handleClick = (event) => {
+        setValue(event.target.value);
+    }
+
+    const handleInputDataFrom = (e) => {
+        const value = e.target.value;
+        setDataFrom(value);
+    };
+
+    const handleInputDataTill = (e) => {
+        const value = e.target.value;
+        setDataTill(value);
+    };
+
+
+
 
     return (
         <section className="cards">
             <div className="sorting">
-                <input className="input" placeholder="Выбор автора" />
-                <input className="input" placeholder="от - до" />
+                <select className='select-list' value={value} onChange={handleClick}>
+                    <option>Мышь</option>
+                    <option>Кот</option>
+                    <option>Сыр</option>
+                    <option>Молоко</option>
+                </select>
+
+                <div className="gallery__bigInput">
+                    <input
+                        className="gallery__dataInput"
+                        name="from"
+                        type="date"
+                        placeholder="От"
+                        onChange={handleInputDataFrom}
+                    />
+                    <input
+                        className="gallery__dataInput"
+                        name="till"
+                        type="date"
+                        placeholder="~ До"
+                        onChange={handleInputDataTill}
+                    />
+                </div>
             </div>
 
-            <ul>
-                <li>
+            <ul className='cards__list list'>
+                <li className='list__item'>
                     <p className="date">
                         12 июня 2022
                     </p>
@@ -22,9 +67,26 @@ export default function Cards() {
                         Может занимать больше трех строк в высоту.
                         В таком случае предложение заканчивается многоточием...
                     </p>
-                    <span className="author">
+                    <button className="author">
                         Иван Иванов
-                    </span>
+                    </button>
+                </li>
+
+                <li className='list__item'>
+                    <p className="date">
+                        12 июня 2022
+                    </p>
+                    <h3 className="cards_title">
+                        Заголовок статьи
+                    </h3>
+                    <p className="cards_info">
+                        Краткое описание статьи или ее начало.
+                        Может занимать больше трех строк в высоту.
+                        В таком случае предложение заканчивается многоточием...
+                    </p>
+                    <button className="author">
+                        Иван Иванов
+                    </button>
                 </li>
             </ul>
         </section>
